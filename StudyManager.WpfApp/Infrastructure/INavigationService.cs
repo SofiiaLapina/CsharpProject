b455abaebaab8170c69f8798b5214ffcd0ad1a5d
Ярослав Бондар <ya.bondar@ukma.edu.ua>
2026-03-21 23:08:24 +0200

    add navigation (temp!!!!)

---
+++ StudyManager.WpfApp/Infrastructure/INavigationService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudyManager.WpfApp.Infrastructure;
+
+public interface INavigationService
+{
+    void NavigateToSubjectDetails(Guid subjectId);
+    void NavigateToLessonDetails(Guid lessonId);
+    void GoBack();
+}
---
+++ StudyManager.WpfApp/Infrastructure/NavigationService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace StudyManager.WpfApp.Infrastructure;
+
+public sealed class NavigationService : INavigationService
+{
+    private readonly Frame _frame;
+
+    public NavigationService(Frame frame)
+    {
+        _frame = frame;
+    }
+
+    public void NavigateToSubjectDetails(Guid subjectId)
+    {
+        // потом глянешь
+        throw new NotImplementedException("Wire SubjectDetailsPage navigation later");
+    }
+
+    public void NavigateToLessonDetails(Guid lessonId)
+    {
+        // не забудь
+        throw new NotImplementedException("Wire LessonDetailsPage navigation later");
+    }
+
+    public void GoBack()
+    {
+        if (_frame.CanGoBack)
+            _frame.GoBack();
+    }
+}
